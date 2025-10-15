7a5dbbf997734fc182e7b05e2867b38ef50a1c68
Theo Sarhane <matchocpro64600@gmail.com>
2025-10-15 09:39:30 +0200

    input done

--- horserace.py
+++ horserace.py
@@ -1,5 +1,10 @@
 import random
 
+first_condition = lambda x: x not in getKeys(type_of_game)
+second_condition = lambda x : (not x.isdigit()) or (int(x) < 12 or int(x) > 20)
+message = 'Choisis entre tierce, quarte, quinte : '
+message2 = 'Combien de chevaux entre 12 et 20'
+
 vitesse_evolution = {
     0: {1: 0, 2: 1, 3: 1, 4: 1, 5: 2, 6: 2},
     1: {1: 0, 2: 0, 3: 1, 4: 1, 5: 1, 6: 2},
@@ -26,3 +31,9 @@ def user_input(message, condition):
         print("Entrée invalide, réessaie.")
         first_input = input(message)
     return first_input
+
+def getKeys(data: dict):
+    return list(data.keys())
+
+user_type_of_game = user_input(message, first_condition)
+horses_nbr = user_input(message2, second_condition)
